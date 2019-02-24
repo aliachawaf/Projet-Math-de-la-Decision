19bc2e2edd2f392897d85f3e1839e3a30ac9892b
Gestin <gestin.remi@gmail.com>
2019-02-24 22:01:12 +0100

    premiere version

--- algo.py
+++ algo.py
@@ -84,25 +84,6 @@ def nbBinomeTrinome(nbStudents):
     else:
         return 18-(nbStudents-36), nbStudents-36
 
-'''
-# Return the list of binomes created
-def createBinomes(preferences):
-
-    binomes = []
-    nbBinome = nbBinomeTrinome(len(preferences))[0]
-
-    i = 0
-    j = 0
-
-    for i in range(len(preferences)):
-        for j in range(len(preferences)):
-
-            if (preferences[i][j] == 1 and not(j in dict(binomes)) and not(j in dict(binomes).values()) and not(i in dict(binomes)) and not(i in dict(binomes).values())):
-
-                binomes.append((i, j))
-
-    return binomes
-'''
 
 # Parameter : array of authorized preferences
 # Return a list of all possible binomes depending on the authorized preferences
@@ -123,7 +104,7 @@ def listPossibleBinomes(authorizedPref):
 def listCombinationsBinomes(binomes, nbBinomesNeeded):
 
     allCombinations = combinations(binomes, nbBinomesNeeded)
-    listCorrectCombinations = []
+    listCorrectCombinationsBinomes = []
 
     for c in allCombinations:
         listStudents = listStudentOfBinomes(c)
@@ -142,11 +123,87 @@ def listCombinationsBinomes(binomes, nbBinomesNeeded):
 
         if not(hasDoublons):
             # the combination is correct and then we keep it
-            listCorrectCombinations.append(c)
+            listCorrectCombinationsBinomes.append(c)
+
+    return listCorrectCombinationsBinomes
+
+def listCombinationsWithTrinomes(matrice, combinaison):
+
+    listCorrectCombinationsTrinomes = []
+    studentsNotAssigned = []
+    listStudents = listStudentOfBinomes(combinaison)
+    listAllTrinome = []
+    listFinal = []
+
+    for i in range(len(matrice[0])):
+        if i not in listStudents:
+            studentsNotAssigned.append(i)
+
+    for i in range(len(studentsNotAssigned)):
+        for j in range(len(combinaison)):
+            if matrice[studentsNotAssigned[i]][combinaison[j][0]] == 1 and matrice[studentsNotAssigned[i]][combinaison[j][1]]:
+                trinome = (combinaison[j][0], combinaison[j][1], studentsNotAssigned[i])
+                listAllTrinome.append(trinome)
+
+    allCombinations = combinations(listAllTrinome, len(studentsNotAssigned))
+
+    for c in allCombinations:
+        listStudents = listStudentsInTrinomes(c)
+
+        j = 0
+        hasDoublons = False
+
+        while j <= len(matrice) and not (hasDoublons):
+
+            # if the student j appears in more than one trinome of the combination
+            if listStudents.count(j) > 1:
+                hasDoublons = True
+
+            j += 1  # next student
+        # end while : if there is a student appearing more than once OR if we checked all students in the combination
+
+        if not (hasDoublons):
+            # the combination is correct and then we keep it
+            listCorrectCombinationsTrinomes.append(c)
+
+    for c in listCorrectCombinationsTrinomes:
+        listTmp = []
+
+        for trinome in c:
+            listTmp.append(trinome)
+
+        for binome in combinaison:
+            if binomeIsNotInTrinomes(binome, c):
+                listTmp.append(binome)
+
+        listFinal.append(listTmp)
+    return listFinal
 
 
+# return true if the binome is in the list of trinomes
+def binomeIsNotInTrinomes(binome, listTrinomes):
+    result = True
 
-    return listCorrectCombinations
+    for t in listTrinomes:
+        if binome[0] == t[0] or binome[0] == t[1] or binome[0] == t[2]:
+            result = False
+
+    return result
+
+
+def listCorrectCombinations(matrice):
+
+    nbStudents = len(matrice)
+    nbBinomesNeeded = nbBinomeTrinome(nbStudents)[0] + nbBinomeTrinome(nbStudents)[1]
+    listCombinationsWithoutTrinome = listCombinationsBinomes(listPossibleBinomes(matrice), int(nbBinomesNeeded))
+
+    listFinalResult = []
+    for c in listCombinationsWithoutTrinome:
+        listTmp = listCombinationsWithTrinomes(matrice, c)
+        for l in listTmp:
+            listFinalResult.append(l)
+
+    return listFinalResult
 
 
 # Parameter : a list of binomes
@@ -161,6 +218,15 @@ def listStudentOfBinomes(binomes):
 
     return list
 
+def listStudentsInTrinomes(trinomes):
+    list = []
+
+    for i in range(len(trinomes)):
+        list.append(trinomes[i][0])  # First student of the trinome i
+        list.append(trinomes[i][1])  # Second student of the trinome i
+        list.append(trinomes[i][2])  # third student of the trinome i
+
+    return list
 
 # Parameter : list of binomes and array of preferences of students not assigned in a group
 # def createTrinomes(binomes):
@@ -173,14 +239,14 @@ def main():
     rang1 = 5
     rang2 = 5
     i = 0
-    nbBinomesNeeded = nbBinomeTrinome(11)[0] + nbBinomeTrinome(11)[1]
+
     while len(resultList) == 0:
 
         authorizedPref = keepAuthorizedPreferences(listPref[rang1], listPref[rang2])
 
         # if checkIfPossible(authorizedPref):
 
-        resultList = listCombinationsBinomes(listPossibleBinomes(authorizedPref), int(nbBinomesNeeded))
+        resultList = listCorrectCombinations(authorizedPref)
         if len(resultList) == 0:
             print("Pas de résultat pour : " + listPref[rang1] + " " + listPref[rang2])
         if i % 2 ==0:
